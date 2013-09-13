15f653758380b300749fdc2035538f9f1447bd75
firmy <firmy@foxmail.com>
2013-09-13 17:17:46 +0800

    sys moden

---
+++ sys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# Filename: using_sys.py
+
+import sys
+print 'The command line arguments are:'
+for i in sys.argv:
+  print i
+print '\n\r\n\rThe PYTHONPATH is', sys.path, '\n\r' 
+
